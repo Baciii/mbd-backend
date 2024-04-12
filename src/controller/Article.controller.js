de93c0e52c348408c63b130c95042394a04ed150
baciii <baciii759@gmail.com>
2024-04-12 15:55:57 +0800

    feat: Article 模块 init

---
+++ src/controller/Article.controller.js
@@ -0,0 +1,8 @@
+import Joi from 'joi';
+
+import ArticleService from '../service/article.js';
+import apiResult from '../apiResult/index.js';
+
+class ArticleController {}
+
+export default new ArticleController();
